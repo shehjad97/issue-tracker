5ef31b332329c4603935e3c0409e610bc99ed0c4
shehjad97 <shehjadcse@gmail.com>
2024-03-09 12:12:17 +0600

    refactor(client): organize import statements

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from 'react'
 import { Button, Callout, Text, TextField } from '@radix-ui/themes'
-import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
@@ -12,6 +11,12 @@ import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
+import dynamic from 'next/dynamic';
+
+const SimpleMDE = dynamic(
+    () => import('react-simplemde-editor'),
+    { ssr: false }
+);
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
